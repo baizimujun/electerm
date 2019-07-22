b7cd8d4b0e22921bc7937db4951e3a6023e207a7
ZHAO Xudong <zxdong@gmail.com>
2019-07-22 13:24:52 +0800

    save

--- app/lib/db-migrate.js
+++ app/lib/db-migrate.js
@@ -2,12 +2,12 @@
  * migrate old file based db to nedb
  */
 
-const { resolve } = require('path')
-const db = require('./nedb')
-const { appPath } = require('../utils/app-props')
-const userConfigPath = resolve(appPath, 'electerm-user-config.json')
-const savePath = resolve(appPath, 'electerm-localstorage.json')
+// const { resolve } = require('path')
+// const db = require('./nedb')
+// const { appPath } = require('../utils/app-props')
+// const userConfigPath = resolve(appPath, 'electerm-user-config.json')
+// const savePath = resolve(appPath, 'electerm-localstorage.json')
 
-module.exports = async () => {
+// module.exports = async () => {
 
-}
+// }
